b96e5af0ff3dbfe2c39c44af3f275a02977baea2
Nizomiddin-Valijanov <nizomiddinvalijanov71@gmail.com>
2024-01-31 01:15:23 +0500

    first commit

---
+++ Birinchi dars/index.py
@@ -0,0 +1,31 @@
+# salom = "salom"
+# print(salom)
+
+user = [
+    {
+        "name": "Nizomiddin",
+        "salary": 100,
+    },
+
+    {
+        "name": "Nizomiddin",
+        "salary": 500,
+    },
+
+    {
+        "name": "Nizomiddin",
+        "salary": 900,
+    },
+
+    {
+        "name": "Nizomiddin",
+        "salary": 20,
+    },
+]
+
+print(user[0]["salary"] + user[1]["salary"] + user[2]["salary"] + user[3]["salary"])
+
+a = int(input("a ni kiriting:"))
+b = int(input("b ni kiriting:"))
+
+print("Siz kiritgan sonlar yiqindisi:", a + b)  
---
+++ Ikkinchi dars/ikkinhidars.py
@@ -0,0 +1,14 @@
+a = int(input("1 son:"))
+b = int(input("2 son:"))
+c = int(input("3 son:"))
+sonlar = 0
+if a > 0:
+    sonlar += 1
+    
+if b > 0:
+    sonlar += 1
+    
+if c > 0:
+    sonlar += 1
+
+print(sonlar)
